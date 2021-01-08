60c125e77b970e191102042f7857e15ebe23d437
HuskyNator <HuskyNator@protonmail.ch>
2021-01-08 06:20:24 +0100

    Console tonen/verbergen in plaats van maken.
    Onduidelijk waarom voor ShowWindow 'user32.lib' vereist is terwijl console api dit niet nodig heeft.

--- source/app.d
+++ source/app.d
@@ -1,6 +1,7 @@
 import std.stdio;
 import std.conv;
 import bindbc.glfw;
+import core.sys.windows.windows;
 
 extern (C) void glfw_foutterugroep(int soort, const char* beschrijving) nothrow {
 	try {
@@ -9,25 +10,31 @@ extern (C) void glfw_foutterugroep(int soort, const char* beschrijving) nothrow 
 	}
 }
 
-extern (C) void glfw_toetsterugroep_debug(GLFWwindow* scherm, int toets,
-		int toetsgetal, int handeling, int toevoeging) nothrow {
-	if (toets == GLFW_KEY_GRAVE_ACCENT) {
-		import core.sys.windows.windows;
+debug {
+	private HWND console;
+	private bool console_zichtbaar;
 
-		try {
-			// Hide / Unhide console.
-			AllocConsole();
-			std.stdio.stdout.reopen("CONOUT$", "w");
-			std.stdio.stdin.reopen("CONIN$", "r");
-		} catch (Exception e) {
+	extern (C) void glfw_toetsterugroep_debug(GLFWwindow* scherm, int toets,
+			int toetsgetal, int handeling, int toevoeging) nothrow {
+		if (toets == GLFW_KEY_GRAVE_ACCENT) {
+			ShowWindow(console, console_zichtbaar ? SW_HIDE : SW_RESTORE);
+			glfwFocusWindow(scherm);
+			console_zichtbaar = !console_zichtbaar;
+		} else {
+			// roep normale terugroep
 		}
-	} else {
-		// roep normale terugroep
-	}
 
+	}
 }
 
 void main() {
+	debug {
+		console = GetConsoleWindow();
+		SetWindowPos(console, HWND_BOTTOM, 0, 0, 1920 / 3, 1080 / 3, SWP_HIDEWINDOW);
+	} else {
+		FreeConsole();
+	}
+
 	import bindbc.glfw;
 	import bindbc.opengl;
 	import wiskunde;
@@ -38,8 +45,8 @@ void main() {
 	GLFWwindow* scherm = glfwCreateWindow(1920 / 2, 1080 / 2, "HoekjeD v0.0.0", null, null);
 	assert(scherm != null, "GLFW kon geen scherm aanmaken.");
 	glfwMakeContextCurrent(scherm);
-
 	debug glfwSetKeyCallback(scherm, &glfw_toetsterugroep_debug);
+
 	GLSupport gl_versie = loadOpenGL();
 	assert(gl_versie == GLSupport.gl46, "GL laadt niet: " ~ gl_versie.to!string);
 
