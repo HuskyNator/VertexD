4fd85fbdfc684dc5bc2f28d486b868ea5bb63cbb
HuskyNator <HuskyNator@protonmail.ch>
2022-05-18 03:52:36 +0200

    Zicht UBO
    - row_major, omdat UBO niet automatische transponeer (glsl gebruikt column_major)
    - std140 opdat de structuur van de uniform niet aan wordt gepast, aangezien de data in de buffer in dit geval niet zou kloppen
    - binding 0, hardgecodeerde ubo binding voor zicht

--- source/hoekjed/invoer/gltf.d
+++ source/hoekjed/invoer/gltf.d
@@ -21,23 +21,26 @@ private:
 string standaard_vert = `
 #version 460
 
-layout(location=0)in vec3 h_plek;
-layout(location=1)in vec3 h_normaal;
-layout(location=2)in vec2 h_beeldplek;
+layout(location=0) in vec3 h_plek;
+layout(location=1) in vec3 h_normaal;
+layout(location=2) in vec2 h_beeldplek;
 
-uniform mat4 projectieM;
-uniform mat4 zichtM;
+layout(row_major, std140, binding=0) uniform Zicht {
+	mat4 projectieM;
+	mat4 zichtM;
+	vec3 zichtplek;
+};
+
 uniform mat4 voorwerpM;
-uniform vec3 ziener;
 
 out vec4 gl_Position;
-out vec3 plaats;
+out vec3 plek;
 out vec3 normaal;
 
 void main(){
-	vec4 plaats4 = voorwerpM * vec4(h_plek, 1.0);
-	plaats = plaats4.xyz;
-	gl_Position = projectieM * zichtM * plaats4;
+	vec4 plek4 = voorwerpM * vec4(h_plek, 1.0);
+	plek = plek4.xyz;
+	gl_Position = projectieM * zichtM * plek4;
 	normaal = h_normaal;
 }
 `;
@@ -46,19 +49,23 @@ string standaard_frag = `
 #version 460
 
 uniform vec4 kleur;
-uniform vec3 ziener;
+layout(row_major, std140, binding=0) uniform Zicht {
+	mat4 projectieM;
+	mat4 zichtM;
+	vec3 zichtplek;
+};
 
-in vec3 plaats;
+in vec3 plek;
 in vec3 normaal;
 
 out vec4 u_kleur;
 
 void main(){
 	vec3 licht = vec3(1, 1, 1);
-	vec3 l = normalize(licht-plaats);
+	vec3 l = normalize(licht-plek);
 	vec3 n = normalize(normaal);
 	vec3 r = reflect(-l, n);
-	vec3 z = normalize(ziener-plaats);
+	vec3 z = normalize(zichtplek-plek);
 	float omgeving = 0.1;
 	float diffuus = clamp(dot(n, l), 0, 1);
 	float specular = clamp(pow(dot(z, r), 50), 0, 1);
