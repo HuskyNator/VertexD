b2fb8394193139971a886300be291763695eacac
HuskyNator <HuskyNator@protonmail.ch>
2023-02-15 14:21:52 +0100

    Input improvements
    Add mouse enter

--- source/vertexd/core/window.d
+++ source/vertexd/core/window.d
@@ -17,18 +17,17 @@ struct MousebuttonInput {
 	int button, event, modifier;
 }
 
-struct MousepositionInput {
-	double x, y;
-}
+alias MousepositionInput = Vec!(2, double);
 
-struct MousewheelInput {
-	double x, y;
-}
+alias MousewheelInput = Vec!(2, double);
+
+alias MouseEnterInput = int;
 
 alias KeyCallback = void delegate(KeyInput input) nothrow;
 alias MousebuttonCallback = void delegate(MousebuttonInput input) nothrow;
 alias MousepositionCallback = void delegate(MousepositionInput input) nothrow;
 alias MousewheelCallback = void delegate(MousewheelInput input) nothrow;
+alias MouseEnterCallback = void delegate(MouseEnterInput entered) nothrow;
 
 enum MouseType {
 	NORMAL = GLFW_CURSOR_NORMAL,
@@ -39,8 +38,13 @@ enum MouseType {
 class Window {
 	// Window Properties
 	string name;
-	int width;
-	int height;
+	union {
+		Vec!(2, int) bounds;
+		struct {
+			int width;
+			int height;
+		}
+	}
 	// package GLFWwindow* glfw_window;
 	GLFWwindow* glfw_window;
 	static package Window[GLFWwindow* ] windows;
@@ -53,10 +57,12 @@ class Window {
 	MousebuttonCallback[] mousebuttonCallbacks = [];
 	MousepositionCallback[] mousepositionCallbacks = [];
 	MousewheelCallback[] mousewheelCallbacks = [];
+	MouseEnterCallback[] mouseEnterCallbacks = [];
 	KeyInput[] keyInput = [];
 	MousepositionInput[] mousepositionInput = [];
 	MousebuttonInput[] mousebuttonInput = [];
 	MousewheelInput[] mousewheelInput = [];
+	MouseEnterInput[] mouseEnterInput = [];
 
 	static void setStandardVisible(bool visible) {
 		glfwWindowHint(GLFW_VISIBLE, visible);
@@ -80,6 +86,8 @@ class Window {
 
 	this(string name = "VertexD", int glfw_width = 960, int glfw_height = 540) {
 		this.name = name;
+		this.width = glfw_width;
+		this.height = glfw_height;
 
 		glfwWindowHint(GLFW_SAMPLES, 4); //TODO: instelbaar
 
@@ -98,13 +106,15 @@ class Window {
 		glfwSetMouseButtonCallback(glfw_window, &window_mousebutton_callback);
 		glfwSetCursorPosCallback(glfw_window, &window_mouseposition_callback);
 		glfwSetScrollCallback(glfw_window, &windows_mousewheel_callback);
+		glfwSetCursorEnterCallback(glfw_window, &windows_mouse_enter_callback);
 		// glfwSetWindowSizeCallback(glfw_window, &window_size_callback);
 		glfwSetFramebufferSizeCallback(glfw_window, &windows_size_callback);
 
 		glfwSetCursorPos(glfw_window, 0, 0);
 
 		GLSupport opengl_version = loadOpenGL();
-		enforce(opengl_version == GLSupport.gl46, "OpenGL not loading: " ~ opengl_version.to!string);
+		enforce(opengl_version == GLSupport.gl46, "OpenGL not loading: " ~ opengl_version
+				.to!string);
 		enforce(hasARBBindlessTexture, "No support for bindless textures");
 
 		debug {
@@ -163,6 +173,11 @@ class Window {
 			foreach (MousewheelCallback callback; mousewheelCallbacks)
 				callback(input);
 
+		foreach (MouseEnterInput input; mouseEnterInput)
+			foreach (MouseEnterCallback callback; mouseEnterCallbacks) {
+				callback(input);
+			}
+
 		//WARNING: assumed independence of mouse & keyboard over small time intervals
 	}
 
@@ -180,6 +195,7 @@ class Window {
 		mousebuttonInput = [];
 		mousepositionInput = [];
 		mousewheelInput = [];
+		mouseEnterInput = [];
 	}
 
 	unittest {
@@ -240,6 +256,11 @@ extern (C) void windows_mousewheel_callback(GLFWwindow* glfw_window, double x, d
 	window.mousewheelInput ~= input;
 }
 
+extern (C) void windows_mouse_enter_callback(GLFWwindow* glfw_window, int entered) nothrow {
+	Window window = Window.windows[glfw_window];
+	window.mouseEnterInput ~= entered;
+}
+
 debug {
 	extern (System) void gl_error_callback(GLenum source, GLenum type, GLuint errorID, GLenum severity,
 		GLsizei length, const GLchar* message, const void* userParam) nothrow {
@@ -251,77 +272,77 @@ debug {
 			writeln("Opengl Exception #" ~ errorID.to!string);
 			write("\tSource: ");
 			switch (source) {
-				case GL_DEBUG_SOURCE_API:
-					writeln("OpenGL API");
-					break;
-				case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
-					writeln("Window System API");
-					break;
-				case GL_DEBUG_SOURCE_SHADER_COMPILER:
-					writeln("Shader Compiler");
-					break;
-				case GL_DEBUG_SOURCE_THIRD_PARTY:
-					writeln("Third Party");
-					break;
-				case GL_DEBUG_SOURCE_APPLICATION:
-					writeln("Source Application");
-					break;
-				case GL_DEBUG_SOURCE_OTHER:
-					writeln("Miscellaneous");
-					break;
-				default:
-					assert(false);
+			case GL_DEBUG_SOURCE_API:
+				writeln("OpenGL API");
+				break;
+			case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+				writeln("Window System API");
+				break;
+			case GL_DEBUG_SOURCE_SHADER_COMPILER:
+				writeln("Shader Compiler");
+				break;
+			case GL_DEBUG_SOURCE_THIRD_PARTY:
+				writeln("Third Party");
+				break;
+			case GL_DEBUG_SOURCE_APPLICATION:
+				writeln("Source Application");
+				break;
+			case GL_DEBUG_SOURCE_OTHER:
+				writeln("Miscellaneous");
+				break;
+			default:
+				assert(false);
 			}
 
 			write("\tType: ");
 			switch (type) {
-				case GL_DEBUG_TYPE_ERROR:
-					writeln("Error ╮(. ❛ ᴗ ❛.)╭");
-					break;
-				case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
-					writeln("Deprecated usage");
-					break;
-				case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
-					writeln("Undefined behaviour");
-					break;
-				case GL_DEBUG_TYPE_PORTABILITY:
-					writeln("System portability");
-					break;
-				case GL_DEBUG_TYPE_PERFORMANCE:
-					writeln("Performance Issues");
-					break;
-				case GL_DEBUG_TYPE_MARKER:
-					writeln("\"Command stream annotation\"");
-					break;
-				case GL_DEBUG_TYPE_PUSH_GROUP:
-					writeln("\"Group pushing\"");
-					break;
-				case GL_DEBUG_TYPE_POP_GROUP:
-					writeln("\"Group popping\"");
-					break;
-				case GL_DEBUG_TYPE_OTHER:
-					writeln("Miscellaneous");
-					break;
-				default:
-					assert(false);
+			case GL_DEBUG_TYPE_ERROR:
+				writeln("Error ╮(. ❛ ᴗ ❛.)╭");
+				break;
+			case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+				writeln("Deprecated usage");
+				break;
+			case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+				writeln("Undefined behaviour");
+				break;
+			case GL_DEBUG_TYPE_PORTABILITY:
+				writeln("System portability");
+				break;
+			case GL_DEBUG_TYPE_PERFORMANCE:
+				writeln("Performance Issues");
+				break;
+			case GL_DEBUG_TYPE_MARKER:
+				writeln("\"Command stream annotation\"");
+				break;
+			case GL_DEBUG_TYPE_PUSH_GROUP:
+				writeln("\"Group pushing\"");
+				break;
+			case GL_DEBUG_TYPE_POP_GROUP:
+				writeln("\"Group popping\"");
+				break;
+			case GL_DEBUG_TYPE_OTHER:
+				writeln("Miscellaneous");
+				break;
+			default:
+				assert(false);
 			}
 
 			write("\tSeverity: ");
 			switch (severity) {
-				case GL_DEBUG_SEVERITY_HIGH:
-					writeln("High");
-					break;
-				case GL_DEBUG_SEVERITY_MEDIUM:
-					writeln("Medium");
-					break;
-				case GL_DEBUG_SEVERITY_LOW:
-					writeln("Low");
-					break;
-				case GL_DEBUG_SEVERITY_NOTIFICATION:
-					writeln("Notification (Miscellaneous)");
-					break;
-				default:
-					assert(false);
+			case GL_DEBUG_SEVERITY_HIGH:
+				writeln("High");
+				break;
+			case GL_DEBUG_SEVERITY_MEDIUM:
+				writeln("Medium");
+				break;
+			case GL_DEBUG_SEVERITY_LOW:
+				writeln("Low");
+				break;
+			case GL_DEBUG_SEVERITY_NOTIFICATION:
+				writeln("Notification (Miscellaneous)");
+				break;
+			default:
+				assert(false);
 			}
 
 			writeln("\tMessage: " ~ message.to!string);
